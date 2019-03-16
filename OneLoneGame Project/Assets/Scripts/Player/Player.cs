cd802c3d036875d621db84e7e577e0b00224f5c0
Memellod <pmi61@users.noreply.github.com>
2019-03-17 01:23:01 +0700

    Добавлен персонаж
    +Передвижение персонажа
    +Анимация смены спрайта при передвижении

---
+++ OneLoneGame Project/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject player;
+    public Sprite Up;
+    public Sprite Down;
+    public Sprite Left;
+    public Sprite Right;
+    public int speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = (GameObject)this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Передвижение WASD +  стрелочки */
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                player.transform.position += player.transform.up * speed * Time.deltaTime;
+                player.GetComponent<SpriteRenderer>().sprite = Up;
+            }
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                player.transform.position -= player.transform.up * speed * Time.deltaTime;
+                player.GetComponent<SpriteRenderer>().sprite = Down;
+            }
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                player.transform.position -= player.transform.right * speed * Time.deltaTime;
+                player.GetComponent<SpriteRenderer>().sprite = Left;
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                player.transform.position += player.transform.right * speed * Time.deltaTime;
+                player.GetComponent<SpriteRenderer>().sprite = Right;
+            }
+        }
+    }
+}
