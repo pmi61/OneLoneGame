435645817ef7ddec00bfd6ee671b5e7691c5a0b3
lampadovnikita <lampadovnikita@gmail.com>
2019-03-22 18:52:39 +0700

    Переименовал Character в Player, начал работу над предметами

---
+++ OneLoneGame Project/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    public string name = "Some name";
+    public string description = "Some item.";
+    public int maxInInvintoryCell = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerNear() == true)
+        {
+            attemptAddToPlayer();
+        }
+        else {
+        }
+    }
+
+    private bool attemptAddToPlayer()
+    {
+
+        return true;
+    }
+
+    private bool isPlayerNear()
+    {
+        Debug.Log("Check distance between Item + \"" + this.name + "\" and player");
+
+
+
+        return true;
+    }
+}
