a05af8c657c157325288b1304bc4fb59f78690f6
Memellod <pmi61@users.noreply.github.com>
2019-04-03 00:19:38 +0700

    О С В Е Щ Е Н И Е
    +тень от персонажа
    +тени от предметов
    *нужна доработка расположения доп. предметов для теней

---
+++ OneLoneGame Project/Assets/TileMapShadows.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+[ExecuteInEditMode]
+public class TileMapShadows : MonoBehaviour
+{
+    public TilemapRenderer s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        s.receiveShadows = true;
+        s.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
